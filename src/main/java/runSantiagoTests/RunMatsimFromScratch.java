0e5b30cb4e8572263c5716e79d73110030d74f51
LeoCamus <leocamusc@gmail.com>
2021-06-21 16:14:11 -0400

    Commiting new SantiagoRepo

---
+++ src/main/java/runSantiagoTests/RunMatsimFromScratch.java
@@ -0,0 +1,70 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package runSantiagoTests;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlansConfigGroup.HandlingOfPlansWithoutRoutingMode;
+import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
+import org.matsim.core.config.groups.QSimConfigGroup.TrafficDynamics;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class RunMatsimFromScratch{
+
+
+	public static void main(String[] args) {
+
+		Config config;
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "/home/leocamus/Escritorio/MATSim_FromScratch/v2a/santiago/config_baseCase1pct.xml" );
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
+
+		config.qsim().setTrafficDynamics( TrafficDynamics.kinematicWaves );
+		config.qsim().setSnapshotStyle( SnapshotStyle.kinematicWaves );
+
+		config.plans().setHandlingOfPlansWithoutRoutingMode(HandlingOfPlansWithoutRoutingMode.useMainModeIdentifier);
+		
+		// ---
+		
+		Scenario scenario = ScenarioUtils.loadScenario(config) ;
+		
+		// possibly modify scenario here
+		
+		// ---
+		
+		Controler controler = new Controler( scenario ) ;
+		
+		// possibly modify controler here
+
+		controler.addOverridingModule( new OTFVisLiveModule() ) ;
+
+		
+		// ---
+		
+		controler.run();
+	}
+	
+}
